b8c3654ec48d376e989402cde1cb6afc6ba489dc
Sam Collins <theatrepro11@gmail.com>
2017-01-14 23:18:43 +0000

    Added Settings Screen and linked to Arma Activity

--- app/src/main/java/org/uk/cyberbyte/armaworks/ArmaLifeActivity.java
+++ app/src/main/java/org/uk/cyberbyte/armaworks/ArmaLifeActivity.java
@@ -62,6 +62,7 @@ public class ArmaLifeActivity extends BaseActivity
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
             return true;
         }
 
